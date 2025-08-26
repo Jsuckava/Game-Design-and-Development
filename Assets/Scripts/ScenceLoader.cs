9558fe94c17a913488f7d1389b0be7e2a76efc3a
Jsuckava <pascualpatrick264@gmail.com>
2025-08-26 21:12:12 +0800

    Add full Unity project

---
+++ Assets/Scripts/ScenceLoader.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ScenceLoader : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadSceneAsync(1);
+        Debug.Log("Proceeding to Next Scene.");
+    }
+
+    public void QuitGame() {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        Debug.Log("Quit!");
+#else
+        Application.Quit();
+#endif
+
+    }
+}
