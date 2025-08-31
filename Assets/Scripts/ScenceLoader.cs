844269759d185628f0b0051833523f420d6ee038
Jsuckava <pascualpatrick264@gmail.com>
2025-08-31 18:43:41 +0800

    new update

--- Assets/Scripts/ScenceLoader.cs
+++ Assets/Scripts/ScenceLoader.cs
@@ -9,11 +9,8 @@ public class ScenceLoader : MonoBehaviour
     public void StartGame() {
         SceneManager.LoadSceneAsync(1);
         Debug.Log("Proceeding to Next Scene.");
-        Debug.LogError("Failed to Proceed into next scene");
     }
-
     public void QuitGame() {
-
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
         Debug.Log("Quit!");
